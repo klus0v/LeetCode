4f1e2b769255172c95a8b477b828a2659c101d22
Klusov <klusov@skbkontur.ru>
2024-01-04 21:37:58 +0500

    https://leetcode.com/problems/remove-duplicates-from-sorted-array/

---
+++ Easy/RemoveDuplicatesFromSortedArray_26.cs
@@ -0,0 +1,17 @@
+﻿namespace LeetCode.Easy;
+
+/// https://leetcode.com/problems/remove-duplicates-from-sorted-array/
+public class RemoveDuplicatesFromSortedArray_26
+{
+    public int RemoveDuplicates(int[] nums) {
+        var k = 1;
+        var prev = nums[0];
+        for (var i = 1; i < nums.Length; i++)
+        {
+            if (nums[i] != prev)
+                nums[k++] = nums[i];
+            prev = nums[i];
+        }
+        return k;
+    }
+}
