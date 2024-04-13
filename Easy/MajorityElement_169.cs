462aeb6875dcbba5c0a2e00dbd3e4434177502a8
Klusov <klusov@skbkontur.ru>
2024-04-13 19:21:17 +0500

    https://leetcode.com/problems/majority-element

---
+++ Easy/MajorityElement_169.cs
@@ -0,0 +1,24 @@
+﻿namespace LeetCode.Easy;
+
+//https://leetcode.com/problems/majority-element
+public class MajorityElement_169
+{
+    public int MajorityElement(int[] nums)
+    {
+        var topByCount = 0;
+        var topCount = 0;
+        
+        foreach (var num in nums)
+        {
+            if (topCount == 0)
+                topByCount = num;
+
+            if (num == topByCount)
+                topCount++;
+            else
+                topCount--;
+        }
+
+        return topByCount;
+    }
+}
